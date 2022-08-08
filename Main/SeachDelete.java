064be57c5d51ee4c07662b7885a399f0793b1fbe
Douglas10009 <douglasevertonev@gmail.com>
2022-08-08 20:34:31 -0300

    Arrumação do design do código

--- Main/SeachDelete.java
+++ Main/SeachDelete.java
@@ -1,57 +1,56 @@
 import java.util.ArrayList;
 
 public class SeachDelete {
-    public static int buscarCnpj(int cnpj ,ArrayList<Fornecedores> list_forn) {
-    	for (int i = 0; i < list_forn.size(); i++) {
+	public static int buscarCnpj(int cnpj, ArrayList<Fornecedores> list_forn) {
+		for (int i = 0; i < list_forn.size(); i++) {
 			if (list_forn.get(i).getCnpj() == cnpj) {
 				System.out.println("Posição do CNPJ para excluir: " + i);
 				return i;
 			}
 		}
-    	return -1;
-    }
-    
-    public static void apagarCnpj(int cnpj, ArrayList<Fornecedores> list_forn, boolean cad_Fornecedor) {
-    	int posicao = buscarCnpj(cnpj, list_forn);
-    	System.out.println("Posição do CNPJ: " + posicao); //Degub
-    	
-    	if (posicao != -1) {
+		return -1;
+	}
+
+	public static void apagarCnpj(int cnpj, ArrayList<Fornecedores> list_forn, boolean cad_Fornecedor) {
+		int posicao = buscarCnpj(cnpj, list_forn);
+
+		if (posicao != -1) {
 			list_forn.remove(posicao);
-			System.out.println("\n-----> Fornecedor com o CNPJ " + cnpj + " foi apagado com sucesso. \n"); //ALERT
-            if (list_forn.size() < 1) {
-                cad_Fornecedor = false;
-            } 
+			System.out.println("\n-----> Fornecedor com o CNPJ " + cnpj + " foi apagado com sucesso. \n"); // ALERT
+			if (list_forn.size() < 1) {
+				cad_Fornecedor = false;
+			}
 
-		}else {
-			System.out.println("\n-----> CNPJ Não encontrado\n"); //ALERT
+		} else {
+			System.out.println("\n-----> CNPJ Não encontrado\n"); // ALERT
 		}
-    
-    }
 
-    public static int buscarCodigoFornecedor(int cod_fornecedor ,ArrayList<Fornecedores> list_forn) {
-    	for (int i = 0; i < list_forn.size(); i++) {
+	}
+
+	public static int buscarCodigoFornecedor(int cod_fornecedor, ArrayList<Fornecedores> list_forn) {
+		for (int i = 0; i < list_forn.size(); i++) {
 			if (list_forn.get(i).getCod_fornecedor() == cod_fornecedor) {
 				System.out.println("Posição do código do fornecedor para excluir: " + i);
 				return i;
 			}
 		}
-    	return -1;
-    }
+		return -1;
+	}
 
-    public static void apagarCodigoFornecedor(int cod_fornecedor, ArrayList<Fornecedores> list_forn, boolean cad_Fornecedor) {
-    	int posicao = buscarCodigoFornecedor(cod_fornecedor, list_forn);
-    	System.out.println("Posição do código do fornecedor: " + posicao); //Degub
-    	
-    	if (posicao != -1) {
+	public static void apagarCodigoFornecedor(int cod_fornecedor, ArrayList<Fornecedores> list_forn,
+			boolean cad_Fornecedor) {
+		int posicao = buscarCodigoFornecedor(cod_fornecedor, list_forn);
+
+		if (posicao != -1) {
 			list_forn.remove(posicao);
-			System.out.println("\n-----> Fornecedor com o código " + cod_fornecedor + " foi apagado com sucesso. \n"); //ALERT
-            if (list_forn.size() < 1) {
-                cad_Fornecedor = false;
-            } 
+			System.out.println("\n-----> Fornecedor com o código " + cod_fornecedor + " foi apagado com sucesso. \n"); // ALERT
+			if (list_forn.size() < 1) {
+				cad_Fornecedor = false;
+			}
 
-		}else {
-			System.out.println("\n-----> Código do fornecedor não encontrado\n"); //ALERT
+		} else {
+			System.out.println("\n-----> Código do fornecedor não encontrado\n"); // ALERT
 		}
-    
-    }
+
+	}
 }
--- Main/index.java
+++ Main/index.java
@@ -1,21 +1,21 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class index {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner sc = new Scanner(System.in);
-        // SeachDelete sd = new SeachDelete();
+
 
         clear(); // Limpar a tela
-        System.out.println("--------------------------------------");
 
-        System.out.println("\n----> Seja bem vindo a Logistica e CIA :)");
-        // belo trabalho
+
+        System.out.println("--------------------------------------");
+        System.out.println("\n----> Seja bem vindo a Logistica e CIA :)"); // belo trabalho
         int opcao = 0;
         ArrayList<Fornecedores> list_forn = new ArrayList<Fornecedores>();
-        // ArrayList<String> aulas = new ArrayList<>();
+
+
 
         while (opcao != 6) {
 
@@ -27,9 +27,10 @@ public class index {
             System.out.println("4 - Sistema de transação - Compra e Venda (BREVE)");
             System.out.println("5 - Relatório (BREVE)");
             System.out.println("6 - Sair ");
-
             System.out.print("Digite o número da sua opção: ");
             opcao = sc.nextInt();
+
+
 
             switch (opcao) {
                 case 1: // Logistica
@@ -41,7 +42,9 @@ public class index {
                     // Cadastrar Clientes
                     // Cadastrar Produtos
 
+
                     clear(); // Limpar tela
+
 
                     System.out.println("-- Sistema de logistica --");
                     System.out.println(">  Gerenciamento ");
@@ -52,14 +55,23 @@ public class index {
                     System.out.print("Digite sua opção: ");
                     int opc_gerenciamento = sc.nextInt();
 
+
                     clear(); // Limpar a tela
+
+
 
                     switch (opc_gerenciamento) {
                         case 1:// Gerenciar Fornecedor
 
+
+
                             Fornecedores forn = new Fornecedores();
                             boolean cad_Fornecedor = false;
+
+
                             while (opcao_log_fornecedores != 3) {
+
+
                                 System.out.println("-- Sistema de logistica --");
                                 System.out.println("-> Fornecedores ");
                                 System.out.println("Escolha uma opção -");
@@ -68,55 +80,46 @@ public class index {
                                 System.out.println("3 - Voltar");
                                 System.out.print("Digite o número da sua opção: ");
                                 opcao_log_fornecedores = sc.nextInt();
-
                                 sc.nextLine(); // Tirar enter do buffer
 
+
+
                                 switch (opcao_log_fornecedores) {
-                                    case 1:
+
+
+                                    case 1://Cadastrar fornecedor
+
+
                                         clear(); // Limpar tela
+
+
                                         System.out.println("\n-- Sistema de logistica --");
                                         System.out.println("--> Cadastrando o fornecedor");
                                         System.out.print("Digite o nome do fornecedor: ");
                                         String nome = sc.nextLine();
-
-                                        System.out.println(nome); // degub
-
                                         System.out.print("Digite o endereço do fornecedor: ");
                                         String endereco = sc.nextLine();
-
-                                        System.out.println(endereco); // Debug
-
                                         System.out.print("Digite o CNPJ do fornecedor:  ");
                                         int cnpj = sc.nextInt();
-
-                                        sc.nextLine(); // Debug
-
+                                        sc.nextLine(); // Tirar enter do Buffer
                                         System.out.print("Digite o telefone do fornecedor: ");
                                         String telefone = sc.nextLine();
-
-                                        // System.out.println(telefone);//Degub
-
                                         System.out.print("Digite o código do fornecedor: ");
                                         int cod_fornecedor = sc.nextInt();
+
+
                                         forn = new Fornecedores(nome, cnpj, endereco, telefone, cod_fornecedor);
                                         list_forn.add(forn);
                                         cad_Fornecedor = true;
 
-                                        // Adicionar agora o poder exluir fornecedor ou add mais
 
-                                        System.out.println("\nnome: " + forn.getNome()); // Debug
-                                        System.out.println("endereço: " + forn.getEndereco()); // Debug
-                                        System.out.println("CNPJ: " + forn.getCnpj()); // Debug
-                                        System.out.println("Telefone: " + forn.getTelefone()); // Debug
-                                        System.out.println("código do fornecedor: " + forn.getCod_fornecedor()); // Debug
-                                        System.out.println("Cadastro fornecedor: " + cad_Fornecedor); // Degub
-                                        System.out.println(" "); // Degub
                                         break;
 
-                                    case 2:
+
+                                    case 2: //Excluir Fornecedor
                                         System.out.println(cad_Fornecedor); // Debug
                                         if (cad_Fornecedor == true) {
-                                            System.out.println("\n-- Sistema de logistica --");
+                                            System.out.println("\n \n-- Sistema de logistica --");
                                             System.out.println("--> Excluindo o fornecedor");
                                             System.out.println("1 - CNPJ");
                                             System.out.println("2 - código do fornecedor");
@@ -124,38 +127,37 @@ public class index {
                                                     "Escolha dentre as alternativas para selecionar o método de exclusão: ");
                                             int excluir_opc = sc.nextInt();
 
-                                            switch (excluir_opc) { // tá dando erro ERROR
-                                                case 1:// CNPJ
-                                                    System.out.println(list_forn.size()); // Debug
 
-                                                    System.out.println(list_forn.size()); // Debug
+
+                                            switch (excluir_opc) { 
+                                                case 1:// CNPJ
                                                     System.out.println("\nDigite um dos CNPJ para apagar: ");
 
+
                                                     for (int i = 0; i < list_forn.size(); i++) {
-                                                        System.out.println(list_forn.get(i).getCnpj());// Aq vai mostrar
-                                                        // os cpf para
-                                                        // excluir o
-                                                        // fornecedor
+                                                        System.out.println(list_forn.get(i).getCnpj());// Aq vai mostrar os cpf para excluir o fornecedor
                                                     }
+
+
                                                     System.out.print("\nDigite sua escolha:");
                                                     int esc_CNPJ = sc.nextInt();
-
-                                                    // int posicao = buscarCnpj(esc_CNPJ, list_forn);
-
-                                                    System.out.print("Tem certeza? 1 - Sim, 2 - Não: ");
+                                                    System.out.print("Tem certeza? [1 - Sim, 2 - Não]: ");
                                                     int certeza = sc.nextInt();
+
+
                                                     if (certeza == 1) {
                                                         SeachDelete.apagarCnpj(esc_CNPJ, list_forn, cad_Fornecedor);
 
                                                     } else {
                                                         System.out.println("\n-----> Ação cancelada\n");
                                                         break;
+                                                        
                                                     }
-
                                                     break; // bom dia Douglas tenta apertar ctrl a + delete alt f4
 
+
+
                                                 case 2: // código do fornecedor
-                                                    // for (int i = 0; i < list_forn.size(); i++) {
                                                     System.out.println(
                                                             "\nDigite um dos códigos dos fornecedores para apagar: ");
 
@@ -173,10 +175,10 @@ public class index {
                                                     System.out.print("\nDigite sua escolha:");
                                                     int esc_Fornecedor = sc.nextInt();
 
-                                                    // int posicao = buscarCnpj(esc_CNPJ, list_forn);
 
                                                     System.out.print("Tem certeza? 1 - Sim, 2 - Não: ");
                                                     int certeza2 = sc.nextInt();
+
                                                     if (certeza2 == 1) {
                                                         SeachDelete.apagarCodigoFornecedor(esc_Fornecedor,
                                                                 list_forn, cad_Fornecedor);
@@ -185,11 +187,13 @@ public class index {
                                                         System.out.println("\n-----> Ação cancelada\n");
                                                         break;
                                                     }
-                                                    // }
                                                     break;
+
+
                                                 default:
                                                     break;
                                             }
+
 
                                         } else {
                                             System.out.println(
@@ -197,6 +201,8 @@ public class index {
                                         }
                                         break;
 
+
+                                    
                                     default:
                                         break;
                                 }
@@ -204,32 +210,50 @@ public class index {
                             }
                             break;
 
+
                         case 2: // Gerenciar Cliente
+
                             break;
 
                         case 3: // Gerenciar Produto
+
                             break;
+
+
                         default:
+
                             break;
                     }
                     // clearScreen(); //Função de limpar a tela
 
                     break;
-                case 2:// Controle
+
+
+                case 2://Sistema de Controle
 
                     break;
-                case 3: // atualização de dados
+
+
+                case 3: //Sistema de atualização de dados
 
                     break;
+
+
                 case 4: // Sistema de transação
 
                     break;
+
+
                 case 5: // Relatório
 
                     break;
+
+
                 case 6: // Saída
 
                     break;
+
+
                 default:
                     System.out.println("opção inválida, tente novamente. ");
                     break;
@@ -239,11 +263,15 @@ public class index {
 
     }
 
+
+
     public static void clearScreen() { // Método para limpar a tela
         char esc = 27;
         String clear = esc + "[2J"; // codigo ansi para limpar a tela
         System.out.print(clear);
     }
+
+
 
     public static void clear() throws IOException, InterruptedException { // Método 2 para limpar a tela
         // Limpa a tela no windows, no linux e no MacOS
@@ -252,13 +280,3 @@ public class index {
         else
             Runtime.getRuntime().exec("clear");
     }
-
-}
-
-/*
- * System.out.println("Escolha uma opção -");
- * System.out.println("1 - ");
- * System.out.println("2 -");
- * System.out.print("Digite o número da sua opção: ");
- * opcao_ = sc.nextInt();
- */
